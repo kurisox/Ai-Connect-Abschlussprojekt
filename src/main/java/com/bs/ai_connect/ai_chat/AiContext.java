3215860179743936818cdd50a7878042e6c639f1
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-07 22:38:34 +0100

    implemented addMessage

--- src/main/java/com/bs/ai_connect/ai_chat/AiContext.java
+++ src/main/java/com/bs/ai_connect/ai_chat/AiContext.java
@@ -1,22 +1,25 @@
 package com.bs.ai_connect.ai_chat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 
 import com.bs.ai_connect.dto.MessageDTO;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 public class AiContext {
     private List<MessageDTO> messages;
     
     @Value("${env.data.aiPersona}")
     private String aiPersona;
 
+    public AiContext(){
+        this.messages = new ArrayList<>();
+    }
+
     public boolean addMessage(MessageDTO message){
-        return true;
+        this.messages.add(message);
+        return this.messages.contains(message);
     }
 
     public int resetContent(){
