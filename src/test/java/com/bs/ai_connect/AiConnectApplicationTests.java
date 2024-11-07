d4f0b5f62edc88ad1c6bcab48152ca7ce229de62
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-07 10:17:15 +0100

    initial commit

---
+++ src/test/java/com/bs/ai_connect/AiConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.ai_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
