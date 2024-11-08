ab0205af5414fc002af4ba3a90c5a4a500d123a2
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 11:32:52 +0100

    implemented addMessage and resetContent

--- src/main/java/com/bs/ai_connect/ai_chat/AiContext.java
+++ src/main/java/com/bs/ai_connect/ai_chat/AiContext.java
@@ -7,11 +7,24 @@ import org.springframework.beans.factory.annotation.Value;
 
 import com.bs.ai_connect.dto.MessageDTO;
 
+import lombok.Getter;
+
+@Getter
 public class AiContext {
+
+    @Value("${env.data.devMode}")
+    private boolean devMode;
+
     private List<MessageDTO> messages;
     
     @Value("${env.data.aiPersona}")
     private String aiPersona;
+
+    @Value("${env.data.role}")
+    private String role;
+
+    @Value("${env.data.summarizeMsg}")
+    private String summarizeMsg;
 
     public AiContext(){
         this.messages = new ArrayList<>();
@@ -23,14 +36,16 @@ public class AiContext {
     }
 
     public int resetContent(){
-        return 0;
+        this.messages.clear();
+        this.messages.add(new MessageDTO(this.role, this.aiPersona));
+        return this.messages.size();
     }
 
-    public int summarizeMessages(){
+    public int summarizeMessages(String role){
         return 1;
     }
 
-    private String summarize(){
+    private String summarize(String role){
         return "";
     }
 }
