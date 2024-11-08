b294b19f4f52f1b974e4b4d2d69a00e08de84d11
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 15:21:59 +0100

    implemented summarizeTests and added reflection

--- src/test/java/com/bs/ai_connect/ai_chat_tests/AiContextTests.java
+++ src/test/java/com/bs/ai_connect/ai_chat_tests/AiContextTests.java
@@ -3,8 +3,11 @@ package com.bs.ai_connect.ai_chat_tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.bs.ai_connect.ai_chat.AiContext;
@@ -16,9 +19,36 @@ public class AiContextTests {
     private AiContext aiContext;
     private String message = "Hello, World!";
 
+    @Value("${env.data.mockMode}")
+    private boolean mockMode;
+
+    @Value("${env.data.aiPersona}")
+    private String aiPersona;
+
+    @Value("${env.data.role}")
+    private String role;
+
+    @Value("${env.data.summarizedMsg}")
+    private String summarizeMsg;
+
+    @Value("${env.data.userRole}")
+    private String userRole;
+
     @BeforeEach
-    public void setup(){
+    public void setup() {
         this.aiContext = new AiContext();
+        Field[] fields = aiContext.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+        }
+        try {
+            fields[0].set(aiContext, mockMode);
+            fields[3].set(aiContext, aiPersona);
+            fields[4].set(aiContext, role);
+            fields[5].set(aiContext, summarizeMsg);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -30,15 +60,23 @@ public class AiContextTests {
 
     @Test
     public void testResetContent() {
-        aiContext.addMessage(new MessageDTO(this.aiContext.getRole(), this.message));   
+        aiContext.addMessage(new MessageDTO(this.aiContext.getRole(), this.message));
         assertEquals(1, aiContext.resetContent());
     }
 
     @Test
-    public void testSummarizeMessages() {
+    public void testSummarizeMessages1() {
         aiContext.addMessage(new MessageDTO(this.aiContext.getRole(), this.message));
         aiContext.addMessage(new MessageDTO(this.aiContext.getRole(), this.message));
-        assertEquals(1, aiContext.summarizeMessages());
+        assertEquals(1, aiContext.summarizeMessages(userRole));
     }
-    
+
+    @Test
+    public void testSummarizeMessages2() {
+        aiContext.addMessage(new MessageDTO(this.aiContext.getRole(), this.message));
+        aiContext.addMessage(new MessageDTO(this.aiContext.getRole(), this.message));
+        MessageDTO message = new MessageDTO(this.aiContext.getRole(), this.message);
+        assertEquals(message, aiContext.getMessages().get(0));
+    }
+
 }
