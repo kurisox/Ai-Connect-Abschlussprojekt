54a337f2c3ae8e559acffca14fd6811f3ec00ae8
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 15:42:09 +0100

    added response message

--- src/test/java/com/bs/ai_connect/ai_chat_tests/AiSingleChatTests.java
+++ src/test/java/com/bs/ai_connect/ai_chat_tests/AiSingleChatTests.java
@@ -22,6 +22,6 @@ public class AiSingleChatTests {
     public void testAskAi_Success() {
         String content = "Test content";
         String response = aiSingleChat.askAi(content);
-        assertEquals("", response);
+        assertEquals("Hello from AI", response);
     }
 }
