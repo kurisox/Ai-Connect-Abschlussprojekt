01281fb5230e804bb1473721cef3a914c4dcd746
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 12:43:15 +0100

    implemented mapResponse

--- src/main/java/com/bs/ai_connect/mapper/AiResponseMapper.java
+++ src/main/java/com/bs/ai_connect/mapper/AiResponseMapper.java
@@ -1,6 +1,10 @@
 package com.bs.ai_connect.mapper;
 
+import java.io.IOException;
+
 import com.bs.ai_connect.dto.ResponseDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import okhttp3.ResponseBody;
@@ -13,14 +17,16 @@ public class AiResponseMapper {
         this.objectMapper = new ObjectMapper();
     }
 
-    public static AiResponseMapper getInstance(){
-        if (aiResponseMapper == null){
+    public static AiResponseMapper getInstance() {
+        if (aiResponseMapper == null) {
             aiResponseMapper = new AiResponseMapper();
         }
         return aiResponseMapper;
     }
 
-    public static ResponseDTO mapResponse(ResponseBody responseBody){
-        return null;
+    public static ResponseDTO mapResponse(ResponseBody responseBody)
+            throws JsonMappingException, JsonProcessingException, IOException {
+        ResponseDTO responseDTO = getInstance().objectMapper.readValue(responseBody.string(), ResponseDTO.class);
+        return responseDTO;
     }
 }
