3a5a45815ffc871c8b987e42145e9257939fa736
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 19:26:57 +0100

    implemented post route

--- src/main/java/com/bs/ai_connect/controller/SimpleAnswerController.java
+++ src/main/java/com/bs/ai_connect/controller/SimpleAnswerController.java
@@ -1,18 +1,28 @@
 package com.bs.ai_connect.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bs.ai_connect.ai_chat.AiChatCompletion;
+import com.bs.ai_connect.ai_chat.AiSingleChat;
+
 
 @RestController
 @RequestMapping("/api/simple-answer")
 public class SimpleAnswerController {
 
+    private AiChatCompletion simpleAnswer;
 
-    @GetMapping("/")
-    public String postAnswer(){
-        return "Hello World";
+    public SimpleAnswerController(){
+        this.simpleAnswer = new AiSingleChat();
+    }
+
+    @PostMapping("/")
+    public String postAnswer(@RequestParam String question){
+        String answer = simpleAnswer.askAi(question);
+        return answer;
     }
 
 }
