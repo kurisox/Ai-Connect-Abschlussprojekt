20d271c6a7e533e04fd15ded411238c4ea8c2b43
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 11:41:16 +0100

    adapted setup and deleted integration test

--- src/test/java/com/bs/ai_connect/ai_chat_tests/AiRequesterTests.java
+++ src/test/java/com/bs/ai_connect/ai_chat_tests/AiRequesterTests.java
@@ -1,28 +1,47 @@
 package com.bs.ai_connect.ai_chat_tests;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.lang.reflect.Field;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.bs.ai_connect.ai_chat.AiRequester;
 
 import okhttp3.Request;
-import okhttp3.ResponseBody;
+
 
 @SpringBootTest
 public class AiRequesterTests {
     private AiRequester aiRequester;
 
     @Value("${env.data.api_endpoint}")
-    private String apiEndpoint;
+    private String api_endpoint;
+
+    @Value("${env.data.content_type}")
+    private String content_type;
+
+    @Value("${env.data.json}")
+    private String json;
 
     @BeforeEach
     public void setUp() {
         aiRequester = new AiRequester();
+        Field[] fields = aiRequester.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+        }
+        try {
+            fields[0].set(aiRequester, true);
+            fields[1].set(aiRequester, api_endpoint);
+            fields[2].set(aiRequester, content_type);
+            fields[3].set(aiRequester, json);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -31,13 +50,4 @@ public class AiRequesterTests {
         Request request = aiRequester.requestBuilder(content);
         assertNotNull(request);
     }
-
-
-    @Test
-    public void testApiCall_Success() {
-        Request request = new Request.Builder().url(apiEndpoint).build();
-        ResponseBody responseBody = aiRequester.apiCall(request);
-        assertNotNull(responseBody);
-    }
-
 }
