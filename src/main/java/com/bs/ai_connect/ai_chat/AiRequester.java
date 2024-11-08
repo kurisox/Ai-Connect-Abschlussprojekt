1566630fff5731452f4cb94977c422639ec031f9
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 11:40:41 +0100

    implemented requestBuilder and apiCall

--- src/main/java/com/bs/ai_connect/ai_chat/AiRequester.java
+++ src/main/java/com/bs/ai_connect/ai_chat/AiRequester.java
@@ -2,29 +2,54 @@ package com.bs.ai_connect.ai_chat;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import com.bs.ai_connect.ai_connector.AiConnector;
+
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import okhttp3.Call;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import okhttp3.ResponseBody;
 
 @NoArgsConstructor
+@Getter
 public class AiRequester {
-    @Value("${env.data.devMode}")
-    private boolean devMode;
+    @Value("${env.data.mockMode}")
+    private boolean mockMode;
 
     @Value("${env.data.api_endpoint}")
     private String api_endpoint;
 
     @Value("${env.data.content_type}")
-    private String content_type ;
+    private String content_type;
 
     @Value("${env.data.json}")
     private String json;
 
-    public Request requestBuilder(String content){
-        return null;
+    public Request requestBuilder(String content) {
+        RequestBody requestBody = RequestBody.create(content, MediaType.parse(content_type));
+        Request request = new Request.Builder()
+                .url(api_endpoint)
+                .header(content_type, json)
+                .post(requestBody)
+                .build();
+        return request;
     }
 
-    public ResponseBody apiCall(Request request){
-        return null;
+    public ResponseBody apiCall(Request request) {
+        Call call = AiConnector.getInstance().getHTTP_CLIENT().newCall(request);
+        if(this.mockMode) {
+            return null;
+        }else{
+            try {
+                Response response = call.execute();
+                return response.body();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 }
