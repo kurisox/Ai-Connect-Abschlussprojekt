50dca7635c3372c346130e01c062dd7308606471
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 15:24:20 +0100

    implemented summarize and summazieMessages

--- src/main/java/com/bs/ai_connect/ai_chat/AiContext.java
+++ src/main/java/com/bs/ai_connect/ai_chat/AiContext.java
@@ -1,13 +1,20 @@
 package com.bs.ai_connect.ai_chat;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 
 import com.bs.ai_connect.dto.MessageDTO;
+import com.bs.ai_connect.dto.ResponseDTO;
+import com.bs.ai_connect.mapper.AiResponseMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import lombok.Getter;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 
 @Getter
 public class AiContext {
@@ -16,6 +23,8 @@ public class AiContext {
     private boolean mockMode;
 
     private List<MessageDTO> messages;
+
+    private AiRequester aiRequester;
     
     @Value("${env.data.aiPersona}")
     private String aiPersona;
@@ -28,6 +37,7 @@ public class AiContext {
 
     public AiContext(){
         this.messages = new ArrayList<>();
+        this.aiRequester = new AiRequester();
     }
 
     public boolean addMessage(MessageDTO message){
@@ -42,10 +52,34 @@ public class AiContext {
     }
 
     public int summarizeMessages(String role){
-        return 1;
+        int listSize;
+        String responseMessage = "Context wurde zusammengefasst";
+        if(!this.mockMode){
+            responseMessage = summarize(role);
+            if(responseMessage == null){
+                return -1;
+            }
+        }
+        this.messages.clear();
+        this.messages.add(new MessageDTO(this.role, this.aiPersona + "\n" + responseMessage));
+        listSize = this.messages.size();
+        return listSize;
     }
 
     private String summarize(String role){
-        return "";
+        Request summarizeRequest = aiRequester.requestBuilder(summarizeMsg);
+        ResponseBody summarizeResponse = aiRequester.apiCall(summarizeRequest);
+        ResponseDTO responseDTO;
+        try {
+            responseDTO = AiResponseMapper.mapResponse(summarizeResponse);
+            return responseDTO.choices()[0].message().content();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) { 
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
