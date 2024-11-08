d978593c3b81ab9e7ffd16b3b388ce9d652668c3
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 13:04:04 +0100

    implemented testMapResponse

--- src/test/java/com/bs/ai_connect/mapper_tests/JavaToJSONMapperTests.java
+++ src/test/java/com/bs/ai_connect/mapper_tests/JavaToJSONMapperTests.java
@@ -4,12 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.bs.ai_connect.dto.ContextDTO;
-
+import com.bs.ai_connect.dto.MessageDTO;
 import com.bs.ai_connect.mapper.JavaToJSONMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+
 
 
 
@@ -27,6 +32,15 @@ public class JavaToJSONMapperTests {
 
     @Test
     public void testMapResponse(){
-        assertEquals(new ContextDTO(),JavaToJSONMapper.mapJSON(new ContextDTO()));
+        List<MessageDTO> messages = List.of(new MessageDTO("system","Hello from AI"));
+        String compareable = "{\"model\":\"gpt-3.5-turbo\",\"max_tokens\":1024,\"messages\":[{\"role\":\"system\",\"content\":\"Hello from AI\"}]}";
+        ContextDTO testContext = new ContextDTO("gpt-3.5-turbo",1024, messages);
+        String contextToJSON;
+        try {
+            contextToJSON = JavaToJSONMapper.mapJSON(testContext);
+            assertEquals(compareable,contextToJSON);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
