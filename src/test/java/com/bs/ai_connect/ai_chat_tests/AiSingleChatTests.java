cddf4a5cc8f6e504617c019556a332e80963e5e6
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-08 15:25:20 +0100

    removed unused import

--- src/test/java/com/bs/ai_connect/ai_chat_tests/AiSingleChatTests.java
+++ src/test/java/com/bs/ai_connect/ai_chat_tests/AiSingleChatTests.java
@@ -1,7 +1,6 @@
 package com.bs.ai_connect.ai_chat_tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
