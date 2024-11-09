3f37a0946f4ca3f278b290b869addbf54d1c3186
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-09 11:23:06 +0100

    initial commit

--- src/main/java/com/bs/ai_connect/controller/ServerStatusController.java
+++ src/main/java/com/bs/ai_connect/controller/ServerStatusController.java
@@ -1,5 +1,18 @@
 package com.bs.ai_connect.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/")
 public class ServerStatusController {
 
+    @GetMapping("")
+    public String serverStatus() {
+        return "Server is running";
+    }
+    
 }
