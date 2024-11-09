751f69b67d85aca8b7df1432797390aef3c89ba8
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-09 16:43:54 +0100

    added aiRequester and aiContext bean

--- src/main/java/com/bs/ai_connect/profiles/Dev.java
+++ src/main/java/com/bs/ai_connect/profiles/Dev.java
@@ -2,6 +2,7 @@ package com.bs.ai_connect.profiles;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 
 import com.bs.ai_connect.ai_chat.AiChatCompletion.AiSingleChat;
@@ -13,14 +14,27 @@ import com.bs.ai_connect.ai_chat.AiRequester.AiRequester;
 @Profile("dev")
 public class Dev {
 
+    @Bean(name = "aiRequester")
+    AiRequester aiRequester() {
+        return new AiRequester();
+    }
+
+    @Bean(name = "aiContext")
+    @DependsOn("aiRequester")
+    AiContext aiContext() {
+        return new AiContext(aiRequester());
+    }
+
     @Bean(name = "aiSingleChat")
+    @DependsOn("aiContext")
     AiSingleChat aiSingleChat() {
-        return new AiSingleChat(new AiContext(new AiRequester()), new AiRequester());
+        return new AiSingleChat(aiContext(), aiRequester());
     }
 
     @Bean(name = "aiConversationChat")
+    @DependsOn("aiContext")
     AiSingleChat aiConversationChat() {
-        return new AiSingleChat(new AiContext(new AiRequester()), new AiRequester());
+        return new AiSingleChat(aiContext(), aiRequester());
     }
 
     @Bean(name = "mockChat")
