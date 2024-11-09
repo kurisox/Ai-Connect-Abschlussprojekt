a2e2da1f3992f3058b01cbd794f2e9f494c615ef
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-09 10:57:08 +0100

    implemented askAI

--- src/main/java/com/bs/ai_connect/ai_chat/AiChatCompletion/MockChat.java
+++ src/main/java/com/bs/ai_connect/ai_chat/AiChatCompletion/MockChat.java
@@ -1,10 +1,24 @@
 package com.bs.ai_connect.ai_chat.AiChatCompletion;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class MockChat implements IAiCompletion {
+    @Value("${env.data.shortAnswer}")
+    private String shortAnswer;
+
+    @Value("${env.data.longAnswer}")    
+    private String longAnswer;
 
     @Override
     public String askAI(String question) {
-        return question;
+        if (question.equals("1")) {
+            return shortAnswer;
+        } else {
+            return longAnswer;
+        }
     }
 
 }
