252126e945d67d4a5e4f356d14dd780fdb9710de
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-11-09 12:59:07 +0100

    configured beans for mock profile

--- src/main/java/com/bs/ai_connect/profiles/Mock.java
+++ src/main/java/com/bs/ai_connect/profiles/Mock.java
@@ -1,5 +1,17 @@
 package com.bs.ai_connect.profiles;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.bs.ai_connect.ai_chat.AiChatCompletion.MockChat;
+
+@Configuration
+@Profile("mock")
 public class Mock {
 
+    @Bean(name = "mockChat")
+    MockChat mockChat() {
+        return new MockChat();
+    }
 }
